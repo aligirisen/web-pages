71a611ce367db0d8b8894591806c95f129f786bb
Ali <aliriza.girisen@gmail.com>
2023-05-29 02:54:34 +0300

    Activity-10v10

--- InclassActivities/Activity-10-Slider/slider.js
+++ InclassActivities/Activity-10-Slider/slider.js
@@ -1,16 +1,18 @@
-$(document).ready(function() {
-        	$("#slider").bxSlider({
-			  auto: true,
-			  minSlides: 2,
-			  maxSlides: 2,
-			  slideWidth: 250,
-			  slideMargin: 10,
-			  randomStart: true,
-			  moveSlides: 1,
-			  minSlides: 1,
-			  maxSlides: 1,
-			  pause: 3000,
-			  pagerCustom: '#pager',
-			  pagerType: 'short'
-			});
-    	});
+
+
+$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		minSlides: 2,
+		maxSlides: 2,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		moveSlides: 1,
+		minSlides: 1,
+		maxSlides: 1,
+		pause: 3000,
+		pagerCustom: '#pager',
+		pagerType: 'short'
+	});
+});
